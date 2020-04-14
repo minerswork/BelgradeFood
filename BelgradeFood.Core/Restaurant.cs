0e77c8bf60aff04e49814e3b6f0a4ef43c888dfe
miner <markopaviccsharp@gmail.com>
2020-04-14 11:19:05 +0200

    Add project files.

---
+++ BelgradeFood.Core/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿namespace BelgradeFood.Core
+{
+    public enum CuisineType { None,Mexican,Indian,Italian }
+
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public CuisineType Cuisine { get; set; }
+
+    }
+
+
+}
---
+++ BelgradeFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace BelgradeFood
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration _config;
+
+        public string Message { get; set; }
+
+        public ListModel(IConfiguration config)
+        {
+            _config = config;
+        }
+        public void OnGet()
+        {
+            
+            Message = _config["Message"];
+        }
+    }
+}
